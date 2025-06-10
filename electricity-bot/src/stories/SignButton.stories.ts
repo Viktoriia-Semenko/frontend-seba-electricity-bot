47dddfd6041eb7ebdce8a4eed9281be131baff62
KravchenkoKseniia <kkravchenko@kse.org.ua>
2025-06-10 10:47:43 +0300

    add SignButton component with styles and Storybook stories

---
+++ electricity-bot/src/stories/SignButton.stories.ts
@@ -0,0 +1,32 @@
+﻿import { SignButton } from '../Components/SignButton/SignButton.tsx';
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import { fn } from 'storybook/test';
+
+const meta: Meta<typeof SignButton> = {
+    title: 'Example/SignButton',
+    component: SignButton,
+    tags: ['autodocs'],
+    parameters: {
+        layout: 'fullscreen',
+    },
+    args: {
+        title: 'Add',
+        isDisabled: false,
+        onClick: fn(() => alert('Register/Login clicked'))
+    },
+};
+
+export default meta;
+type Story = StoryObj<typeof SignButton>;
+
+export const SignIn: Story = {
+    args: {
+        title: 'Sign In'
+    },
+};
+
+export const SignUp: Story = {
+    args: {
+        title: 'Continue'
+    },
+};
