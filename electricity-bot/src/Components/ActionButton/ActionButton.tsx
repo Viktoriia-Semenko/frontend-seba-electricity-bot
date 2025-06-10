81427b2176b3ee819630eb9e9008d76e7c989bee
KravchenkoKseniia <kkravchenko@kse.org.ua>
2025-06-10 10:33:52 +0300

    add ActionButton component with styles and Storybook stories

---
+++ electricity-bot/src/Components/ActionButton/ActionButton.tsx
@@ -0,0 +1,13 @@
+﻿import './styles.css';
+
+interface ActionButtonProps {
+    title: string;
+    onClick?: () => void;
+    isDisabled?: boolean;
+}
+
+export const ActionButton = ({ title, onClick, isDisabled}: ActionButtonProps) => {
+    return (
+        <button className='action-button' onClick={onClick} disabled={isDisabled}>{title}</button>
+    );
+};
---
+++ electricity-bot/src/stories/ActionButton.stories.ts
@@ -0,0 +1,45 @@
+﻿import { ActionButton } from '../Components/ActionButton/ActionButton.tsx';
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import { fn } from 'storybook/test';
+
+const meta: Meta<typeof ActionButton> = {
+    title: 'Example/ActionButton',
+    component: ActionButton,
+    tags: ['autodocs'],
+    parameters: {
+        layout: 'fullscreen',
+    },
+    args: {
+        title: 'Add',
+        isDisabled: false,
+        onClick: fn(() => alert('Device clicked!'))
+    },
+};
+
+export default meta;
+type Story = StoryObj<typeof ActionButton>;
+
+export const Edit: Story = {
+    args: {
+        title: 'Edit'
+    },
+};
+
+export const Save: Story = {
+    args: {
+        title: 'Save'
+    },
+};
+
+export const Add: Story = {
+    args: {
+        title: 'Add'
+    },
+};
+
+export const Disabled: Story = {
+    args: {
+        title: 'Delete',
+        isDisabled: true
+    },
+};
