039503e39fe99f671ed8c5fc763a5cdd8b825a31
KravchenkoKseniia <kkravchenko@kse.org.ua>
2025-06-12 00:38:34 +0300

    add RegistrationForm component with styles and Storybook stories

---
+++ electricity-bot/src/Components/RegistrationForm/RegistrationForm.tsx
@@ -0,0 +1,141 @@
+﻿import styles from './RegistrationForm.module.css'
+
+import {
+    //ConfigProvider,
+    Form,
+    Input,
+    // Select,
+} from 'antd';
+
+import {SignButton} from "../SignButton/SignButton.tsx";
+import * as React from "react";
+
+// const { Option } = Select;
+
+interface RegistrationFormProps {
+    registerUrl?: string;
+}
+
+export const RegistrationForm: React.FC<RegistrationFormProps> = ({registerUrl = "#"} : RegistrationFormProps) => {
+    const [form] = Form.useForm();
+
+    const onFinish = (values: unknown) => {
+        console.log('Received values of form: ', values);
+    };
+
+    return (
+        <Form
+            form={form}
+            name="register"
+            onFinish={onFinish}
+            style={{ maxWidth: 600 }}
+            scrollToFirstError
+        >
+            <Form.Item
+                name="first name"
+                style={{marginBottom: 47}}
+                rules={[
+                    {
+                        required: true,
+                        message: 'Please input your first name!',
+                        whitespace: true,
+                    },
+                ]}
+            >
+                <Input className={styles.input} placeholder="First name"/>
+            </Form.Item>
+
+            <Form.Item
+                name="last name"
+                style={{marginBottom: 47}}
+                rules={[
+                    {
+                        required: true,
+                        message: 'Please input your last name!',
+                        whitespace: true,
+                    },
+                ]}
+            >
+                <Input className={styles.input} placeholder="Last name" />
+            </Form.Item>
+
+            <Form.Item
+                name="email"
+                style={{marginBottom: 47}}
+                rules={[
+                    {
+                        type: 'email',
+                        message: 'The input is not valid E-mail!',
+                    },
+                    {
+                        required: true,
+                        message: 'Please input your E-mail!',
+                    },
+                ]}
+            >
+                <Input className={styles.input} placeholder="Email" />
+            </Form.Item>
+
+            <Form.Item
+                name="password"
+                style={{marginBottom: 47}}
+                rules={[
+                    {
+                        required: true,
+                        message: 'Please input your password!',
+                    },
+                ]}
+                hasFeedback
+            >
+                <Input.Password className={styles.input} placeholder="Password" />
+            </Form.Item>
+
+            <Form.Item
+                name="confirm"
+                dependencies={['password']}
+                style={{marginBottom: 35}}
+                hasFeedback
+                rules={[
+                    {
+                        required: true,
+                        message: 'Please confirm your password!',
+                    },
+                    ({ getFieldValue }) => ({
+                        validator(_, value) {
+                            if (!value || getFieldValue('password') === value) {
+                                return Promise.resolve();
+                            }
+                            return Promise.reject(new Error('The new password that you entered do not match!'));
+                        },
+                    }),
+                ]}
+            >
+                <Input.Password className={styles.input} placeholder="Confirm your password" />
+            </Form.Item>
+
+            {/*<Form.Item*/}
+            {/*    name="gender"*/}
+            {/*    rules={[{ required: true, message: 'Please select gender!' }]}*/}
+            {/*>*/}
+            {/*    <ConfigProvider*/}
+            {/*        select={{className: styles.genderSelect}}*/}
+            {/*    >*/}
+            {/*        <Select className={styles.genderSelect} placeholder="Gender">*/}
+            {/*            <Option value="male">Male</Option>*/}
+            {/*            <Option value="female">Female</Option>*/}
+            {/*            <Option value="other">Other</Option>*/}
+            {/*        </Select>*/}
+            {/*    </ConfigProvider>*/}
+            {/*</Form.Item>*/}
+
+            <Form.Item
+                style={{marginTop:0, marginBottom: 36, textAlign: 'center'}}>
+                <p>Already have an account? <a href={registerUrl}>Login</a></p>
+            </Form.Item>
+
+            <Form.Item>
+                <SignButton title="Sign Up" htmlType="submit" />
+            </Form.Item>
+        </Form>
+    );
+};
---
+++ electricity-bot/src/stories/RegistrationForm.stories.ts
@@ -0,0 +1,16 @@
+﻿import type { Meta, StoryObj } from '@storybook/react-vite';
+
+import { RegistrationForm } from '../Components/RegistrationForm/RegistrationForm.tsx';
+
+const meta = {
+    title: 'Example/RegistrationForm',
+    component: RegistrationForm,
+    tags: ['autodocs'],
+    parameters: {
+        layout: 'fullscreen',
+    }
+} satisfies Meta<typeof RegistrationForm>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+export const Default: Story = {};
