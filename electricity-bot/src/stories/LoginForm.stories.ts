ce599ed1e4312707c8ab34c77f1c16cc524b2ce1
KravchenkoKseniia <kkravchenko@kse.org.ua>
2025-06-11 21:39:33 +0300

    add LoginForm component with styles and Storybook stories

---
+++ electricity-bot/src/stories/LoginForm.stories.ts
@@ -0,0 +1,16 @@
+﻿import type { Meta, StoryObj } from '@storybook/react-vite';
+
+import { LoginForm } from '../Components/LoginForm/LoginForm.tsx';
+
+const meta = {
+    title: 'Example/LoginForm',
+    component: LoginForm,
+    tags: ['autodocs'],
+    parameters: {
+        layout: 'fullscreen',
+    }
+} satisfies Meta<typeof LoginForm>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+export const Default: Story = {};
