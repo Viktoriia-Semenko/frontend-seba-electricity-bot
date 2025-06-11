b84a6271ef9a321ae931778ce9c45ede3798fee0
KravchenkoKseniia <kkravchenko@kse.org.ua>
2025-06-11 22:21:13 +0300

    add Header component with styles and Storybook stories

---
+++ electricity-bot/src/stories/Header.stories.ts
@@ -0,0 +1,32 @@
+﻿import type {Meta, StoryObj} from "@storybook/react-vite";
+
+import {Header} from "../Components/Header/Header.tsx";
+
+const meta = {
+    title: 'Example/Header',
+    component: Header,
+    tags: ['autodocs'],
+    parameters: {
+        layout: 'fullscreen',
+    },
+    args: {
+        title: "Header Title",
+    },
+} satisfies Meta<typeof Header>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Main: Story = {
+    args: {
+        title: "Main",
+        pageType: 'main'
+    },
+};
+
+export const Login: Story = {
+    args: {
+        title: "Login",
+        pageType: 'login'
+    },
+}
