4881ed5fe41b1395d106afc249d7dbcba3bf227f
KravchenkoKseniia <kkravchenko@kse.org.ua>
2025-06-16 18:05:18 +0300

    refactor RegistrationForm component to use native form elements and add validation error messages

--- electricity-bot/src/Components/RegistrationForm/RegistrationForm.tsx
+++ electricity-bot/src/Components/RegistrationForm/RegistrationForm.tsx
@@ -1,139 +1,112 @@
-﻿import styles from './RegistrationForm.module.css'
-
-import {
-    //ConfigProvider,
-    Form,
-    Input,
-    // Select,
-} from 'antd';
-
+﻿import React from 'react';
+import type {FormEventHandler} from 'react';
+import styles from './RegistrationForm.module.css';
+import {Header} from "../Header/Header.tsx";
 import {SignButton} from "../SignButton/SignButton.tsx";
-import * as React from "react";
 
-// const { Option } = Select;
+type RegistrationFormProps = {
+    onSub:  FormEventHandler<HTMLFormElement>;
+    isDis?: boolean;
+    firstNameRef: React.RefObject<HTMLInputElement>;
+    lastNameRef: React.RefObject<HTMLInputElement>;
+    emailRef: React.RefObject<HTMLInputElement>;
+    genderRef: React.RefObject<HTMLSelectElement>;
+    passwordRef: React.RefObject<HTMLInputElement>;
+};
 
-interface RegistrationFormProps {
-    onSub: React.FormEventHandler<HTMLFormElement>;
-    isDisabled?: boolean;
-    registerUrl?: string;
-}
+export const RegistrationForm: React.FC<RegistrationFormProps> = ({onSub, isDis, firstNameRef, lastNameRef, emailRef, genderRef, passwordRef}) => {
+    const [firstNameError, setFirstNameError] = React.useState<string>('');
+    const [lastNameError, setLastNameError] = React.useState<string>('');
+    const [emailError, setEmailError] = React.useState<string>('');
+    const [passwordError, setPasswordError] = React.useState<string>('');
 
-export const RegistrationForm: React.FC<RegistrationFormProps> = ({registerUrl = "#", onSub, isDisabled = false} : RegistrationFormProps) => {
-    const [form] = Form.useForm();
+    const validateForm = () => {
+        let isValid = true;
+
+        const firstName = firstNameRef.current?.value.trim() || '';
+        const lastName = lastNameRef.current?.value.trim() || '';
+        const email = emailRef.current?.value.trim() || '';
+        const password = passwordRef.current?.value.trim() || '';
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if (!firstName) {
+            setFirstNameError('First name is required');
+            isValid = false;
+        }
+        else {
+            setFirstNameError('');
+        }
+
+        if (!lastName) {
+            setLastNameError('Last name is required');
+            isValid = false;
+        }
+        else {
+            setLastNameError('');
+        }
+
+        if (!email || !emailRegex.test(email)) {
+            setEmailError('Email is required');
+            isValid = false;
+        }
+        else {
+            setEmailError('');
+        }
+
+        if (!password || password.length < 8) {
+            setPasswordError('Password must be at least 8 characters long');
+            isValid = false;
+        }
+        else {
+            setPasswordError('');
+        }
+
+        return isValid;
+    }
+
+    const handleSubmit: FormEventHandler<HTMLFormElement> = (event) => {
+        event.preventDefault();
+        if (validateForm()) {
+            onSub(event);
+        }
+    };
 
     return (
-        <Form
-            form={form}
-            name="register"
-            onFinish={onSub}
-            style={{ maxWidth: 600 }}
-            scrollToFirstError
-        >
-            <Form.Item
-                name="first name"
-                style={{marginBottom: 47}}
-                rules={[
-                    {
-                        required: true,
-                        message: 'Please input your first name!',
-                        whitespace: true,
-                    },
-                ]}
-            >
-                <Input className={styles.input} placeholder="First name"/>
-            </Form.Item>
-
-            <Form.Item
-                name="last name"
-                style={{marginBottom: 47}}
-                rules={[
-                    {
-                        required: true,
-                        message: 'Please input your last name!',
-                        whitespace: true,
-                    },
-                ]}
-            >
-                <Input className={styles.input} placeholder="Last name" />
-            </Form.Item>
-
-            <Form.Item
-                name="email"
-                style={{marginBottom: 47}}
-                rules={[
-                    {
-                        type: 'email',
-                        message: 'The input is not valid E-mail!',
-                    },
-                    {
-                        required: true,
-                        message: 'Please input your E-mail!',
-                    },
-                ]}
-            >
-                <Input className={styles.input} placeholder="Email" />
-            </Form.Item>
-
-            <Form.Item
-                name="password"
-                style={{marginBottom: 47}}
-                rules={[
-                    {
-                        required: true,
-                        message: 'Please input your password!',
-                    },
-                ]}
-                hasFeedback
-            >
-                <Input.Password className={styles.input} placeholder="Password" />
-            </Form.Item>
-
-            <Form.Item
-                name="confirm"
-                dependencies={['password']}
-                style={{marginBottom: 35}}
-                hasFeedback
-                rules={[
-                    {
-                        required: true,
-                        message: 'Please confirm your password!',
-                    },
-                    ({ getFieldValue }) => ({
-                        validator(_, value) {
-                            if (!value || getFieldValue('password') === value) {
-                                return Promise.resolve();
-                            }
-                            return Promise.reject(new Error('The new password that you entered do not match!'));
-                        },
-                    }),
-                ]}
-            >
-                <Input.Password className={styles.input} placeholder="Confirm your password" />
-            </Form.Item>
-
-            {/*<Form.Item*/}
-            {/*    name="gender"*/}
-            {/*    rules={[{ required: true, message: 'Please select gender!' }]}*/}
-            {/*>*/}
-            {/*    <ConfigProvider*/}
-            {/*        select={{className: styles.genderSelect}}*/}
-            {/*    >*/}
-            {/*        <Select className={styles.genderSelect} placeholder="Gender">*/}
-            {/*            <Option value="male">Male</Option>*/}
-            {/*            <Option value="female">Female</Option>*/}
-            {/*            <Option value="other">Other</Option>*/}
-            {/*        </Select>*/}
-            {/*    </ConfigProvider>*/}
-            {/*</Form.Item>*/}
-
-            <Form.Item
-                style={{marginTop:0, marginBottom: 36, textAlign: 'center'}}>
-                <p>Already have an account? <a href={registerUrl}>Login</a></p>
-            </Form.Item>
-
-            <Form.Item>
-                <SignButton isDisabled={isDisabled} title="Sign Up" htmlType="submit" />
-            </Form.Item>
-        </Form>
+        <div className={styles.container}>
+            <Header title={"Registration"} pageType={"login"} />
+            <form onSubmit={handleSubmit} className={styles.form}>
+                <div className={styles.firstName}>
+                    <input type="text" className={styles.input} ref={firstNameRef} placeholder={"First Name"} />
+                    {firstNameError && <p className={styles.error}>* {firstNameError}</p>}
+                </div>
+                <div className={styles.lastName}>
+                    <input type="text" className={styles.input} ref={lastNameRef} placeholder={"Last Name"} />
+                    {lastNameError && <p className={styles.error}>* {lastNameError}</p>}
+                </div>
+                <div className={styles.gender}>
+                    <select className={styles.select} ref={genderRef} aria-placeholder={"Gender"}>
+                        <option value="">Select Gender</option>
+                        <option value="male">Male</option>
+                        <option value="female">Female</option>
+                        <option value="other">Other</option>
+                    </select>
+                </div>
+                <div className={styles.email}>
+                    <input type="email" className={styles.input} ref={emailRef} placeholder={"Email"} />
+                    {emailError && <p className={styles.error}>* {emailError}</p>}
+                </div>
+                <div className={styles.password}>
+                    <input type="password" className={styles.input} ref={passwordRef} placeholder={"Password"} />
+                    {passwordError && <p className={styles.error}>* {passwordError}</p>}
+                </div>
+                <div className={styles.btnContainer}>
+                    <p className={styles.privacyPolicy}>
+                        Already have an account? <a href="/login" className={styles.link}>Login</a>
+                    </p>
+                    <SignButton htmlType={"submit"} title={'Sign up'} isDisabled={isDis} />
+                </div>
+            </form>
+        </div>
     );
 };
