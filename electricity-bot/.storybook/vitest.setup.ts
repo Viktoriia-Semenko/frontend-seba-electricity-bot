f9d7b9081d6f12e05c79460ddda6d96e949f0dfa
Viktoriia-Semenko <150340301+Viktoriia-Semenko@users.noreply.github.com>
2025-06-22 22:12:31 +0300

    added vitetest for test login, registration and token

--- electricity-bot/.storybook/vitest.setup.ts
+++ electricity-bot/.storybook/vitest.setup.ts
@@ -1,7 +1,27 @@
 import * as a11yAddonAnnotations from "@storybook/addon-a11y/preview";
 import { setProjectAnnotations } from '@storybook/react-vite';
 import * as projectAnnotations from './preview';
+import { beforeEach, vi } from 'vitest';
 
 // This is an important step to apply the right configuration when testing your stories.
 // More info at: https://storybook.js.org/docs/api/portable-stories/portable-stories-vitest#setprojectannotations
 setProjectAnnotations([a11yAddonAnnotations, projectAnnotations]);
+
+beforeEach(() => {
+    const store: Record<string, string> = {};
+
+    vi.stubGlobal('localStorage', {
+        getItem: (key: string) => store[key] || null,
+        setItem: (key: string, value: string) => {
+            store[key] = value;
+        },
+        removeItem: (key: string) => {
+            delete store[key];
+        },
+        clear: () => {
+            for (const key in store) {
+                delete store[key];
+            }
+        },
+    });
+});
