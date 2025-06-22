948c013161a1a9e3863ce56728af017864a61265
Viktoriia-Semenko <150340301+Viktoriia-Semenko@users.noreply.github.com>
2025-06-22 23:24:40 +0300

    fix vite config for testing

--- electricity-bot/vite.config.ts
+++ electricity-bot/vite.config.ts
@@ -1,10 +1,33 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+import storybookTest from '@storybook/addon-vitest/vitest-plugin';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
-    setupFiles: ['./.storybook/vitest.setup.ts'],
+    projects: [
+      // 👇 UNIT tests
+      defineConfig({
+        test: {
+          setupFiles: ['./.storybook/vitest.setup.ts'],
+          include: ['src/**/*.test.ts'],
+        },
+      }),
+
+      // 👇 Storybook browser tests
+      defineConfig({
+        plugins: [storybookTest({ configDir: '.storybook' })],
+        test: {
+          setupFiles: ['./.storybook/vitest.setup.ts'],
+          browser: {
+            enabled: true,
+            provider: 'playwright',
+            headless: true,
+            instances: [{ browser: 'chromium' }],
+          },
+        },
+      }),
+    ],
   },
 });
