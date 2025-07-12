22a1735dfd4186c73cf64e104847b3e4725a98f6
KravchenkoKseniia <kkravchenko@kse.org.ua>
2025-07-12 14:49:09 +0300

    fix: implement protected routes for main page navigation

--- electricity-bot/src/App.tsx
+++ electricity-bot/src/App.tsx
@@ -1,37 +1,43 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
 import { LoginPage } from './pages/LoginPage/LoginPage';
 import { RegisterPage } from './pages/RegisterPage/RegisterPage';
 import { MainLayout } from './pages/Layout/MainLayout';
+import {ProtectedRoute} from "./Components/ProtectedRoute/ProtectedRoute.tsx";
 // import { EmptyStatePage } from './pages/EmptyStatePage/EmptyStatePage';
-import {MainPage} from "./pages/MainPage/MainPage.tsx";
+// import {MainPage} from "./pages/MainPage/MainPage.tsx";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
+                <Route path="/" element={<Navigate to="/home" replace />} />
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/register" element={<RegisterPage />} />
 
-                <Route
-                    path="/home"
-                    element={<MainPage />}
-                />
-                <Route
-                    path="/history"
-                    element={
-                        <MainLayout page="history">
-                            <p style={{ color: 'white' }}>History content</p>
-                        </MainLayout>
-                    }
-                />
-                <Route
-                    path="/settings"
-                    element={
-                        <MainLayout page="settings">
-                            <p style={{ color: 'white' }}>Settings content</p>
-                        </MainLayout>
-                    }
-                />
+                <Route element={<ProtectedRoute />}>
+                    <Route
+                        path="/home"
+                        element={<MainLayout page="home">
+                            <p style={{ color: 'white' }}>Home content</p>
+                        </MainLayout>}
+                    />
+                    <Route
+                        path="/history"
+                        element={
+                            <MainLayout page="history">
+                                <p style={{ color: 'white' }}>History content</p>
+                            </MainLayout>
+                        }
+                    />
+                    <Route
+                        path="/settings"
+                        element={
+                            <MainLayout page="settings">
+                                <p style={{ color: 'white' }}>Settings content</p>
+                            </MainLayout>
+                        }
+                    />
+                </Route>
             </Routes>
         </BrowserRouter>
     );
---
+++ electricity-bot/src/Components/ProtectedRoute/ProtectedRoute.tsx
@@ -0,0 +1,12 @@
+﻿import {Navigate, Outlet} from "react-router-dom";
+import {SESSION_KEY} from "../../constants/session.ts";
+
+export const ProtectedRoute = () => {
+    const token = localStorage.getItem(SESSION_KEY);
+
+    if (!token) {
+        return <Navigate to="/login" replace />;
+    }
+
+    return <Outlet />;
+};
